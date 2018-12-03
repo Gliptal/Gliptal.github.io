6c38af47a0d2f1bcb557e26aab00a3d8ec0d821c
Mattia Affabris <affa@outlook.it>
2018-12-03 14:06:54 +0100

    0.3.1-alpha3+201812031352

--- classsumointegrator_1_1_sumo_1_1_ego.js
+++ classsumointegrator_1_1_sumo_1_1_ego.js
@@ -1,5 +1,6 @@
 var classsumointegrator_1_1_sumo_1_1_ego =
 [
+    [ "subscribe", "classsumointegrator_1_1_sumo_1_1_ego.html#a07ea5fd0fafb10bf579fa92227108374", null ],
     [ "move", "classsumointegrator_1_1_sumo_1_1_ego.html#aa8676a1a22d0fc3e11097ab3a6a8cbf1", null ],
     [ "move", "classsumointegrator_1_1_sumo_1_1_ego.html#a87ee6436139dbce94e65927c43212f43", null ],
     [ "get_id", "classsumointegrator_1_1_sumo_1_1_ego.html#a7cb79b699a8a87688fbea147569fd24a", null ],
